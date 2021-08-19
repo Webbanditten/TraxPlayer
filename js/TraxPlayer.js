c8a575bf271b7aa18f2f270fe6a70f6a620e5165
Patrick Nærvig Wøhlk <patrick@wohlk.com>
2021-08-19 10:59:09 +0200

    Fix position of play button and loop trax when it finishes

--- js/TraxPlayer.js
+++ js/TraxPlayer.js
@@ -138,10 +138,10 @@ class TraxPlayer {
             .then(data => console.log(data));*/
         //var song = "status=0&name=Too lost in the lido&author=Patrick&track1=317,4;408,7;0,1;410,16;413,4;406,4;410,8;412,4&track2=0,2;321,2;443,22;91,2;317,8;443,8;412,2;0,2&track3=0,3;320,2;0,7;414,4;445,4;412,2;323,2;412,4;96,2;412,2;414,4;445,7;448,1;317,4&track4=0,3;324,2;0,6;448,1;0,6;96,2;322,4;96,2;99,2;322,4;412,2;0,2;322,2;96,2;322,2;0,1;324,2;0,3";
         //var song = "status=0&name=Too lost in the lido&author=Patrick&track1=317,4;408,7;0,1;410,16;413,4;406,4;410,8;412,4:2:0,2;321,2;443,22;91,2;317,8;443,8;412,2;0,2:3:0,3;320,2;0,7;414,4;445,4;412,2;323,2;412,4;96,2;412,2;414,4;445,7;448,1;317,4:4:0,3;324,2;0,6;448,1;0,6;96,2;322,4;96,2;99,2;322,4;412,2;0,2;322,2;96,2;322,2;0,1;324,2;0,3&track2=0,2;321,2;443,22;91,2;317,8;443,8;412,2;0,2:3:0,3;320,2;0,7;414,4;445,4;412,2;323,2;412,4;96,2;412,2;414,4;445,7;448,1;317,4:4:0,3;324,2;0,6;448,1;0,6;96,2;322,4;96,2;99,2;322,4;412,2;0,2;322,2;96,2;322,2;0,1;324,2;0,3&track3=0,3;320,2;0,7;414,4;445,4;412,2;323,2;412,4;96,2;412,2;414,4;445,7;448,1;317,4:4:0,3;324,2;0,6;448,1;0,6;96,2;322,4;96,2;99,2;322,4;412,2;0,2;322,2;96,2;322,2;0,1;324,2;0,3&track4=0,3;324,2;0,6;448,1;0,6;96,2;322,4;96,2;99,2;322,4;412,2;0,2;322,2;96,2;322,2;0,1;324,2;0,3";
-        //var song = "status=0&name=test&author=Patrick&track1=4,4;9,2:2:0,5;311,&track2=0,5;311,1:3:0,4;308,1;0,1:4:0,2;307,1;0,3&track3=0,4;308,1;0,1:4:0,2;307,1;0,3&track4=0,2;307,1;0,3";
+        var song = "status=0&name=test&author=Patrick&track1=4,4;9,2:2:0,5;311,&track2=0,5;311,1:3:0,4;308,1;0,1:4:0,2;307,1;0,3&track3=0,4;308,1;0,1:4:0,2;307,1;0,3&track4=0,2;307,1;0,3";
         //var song = "status=0&name=test&author=Patrick&track1=4,12:2:0,5;311,1;0,6:3:0,4;308,1;0,7:4:0,2;307,1;0,9&track2=0,5;311,1;0,6:3:0,4;308,1;0,7:4:0,2;307,1;0,9&track3=0,4;308,1;0,7:4:0,2;307,1;0,9&track4=0,2;307,1;0,9";
 
-        var song = await this.fetchUrl(this.songUrl);
+        //var song = await this.fetchUrl(this.songUrl);
         var urlSearchParams = new URLSearchParams("?" + song);
 
         var track1 = urlSearchParams.get("track1");
@@ -238,47 +238,6 @@ class TraxPlayer {
         } catch (err) {
             console.log("Failed during preload", err);
         }
-
-        /*this.FetchSong().then(function(tracks) {
-            console.log("Song loaded, loading samples");
-            console.log("TRACKS");
-            console.log(tracks);
-            var uniqueSamples = _self.GetUniqueSamples(tracks).map(function(sample) {
-                return _self.GetDuration(sample);
-            });
-
-            Promise.all(uniqueSamples).then(function(richSamples) {
-                for (var i = 0; i < richSamples.length; i++) {
-                    _self.samples[richSamples[i].sample.piece] = richSamples[i];
-                }
-                console.log("All samples loaded")
-
-                for (var i = 0; i < tracks.length; i++) {
-
-
-                    // BUILD Actual Tracks
-                    var actualTrack = [];
-                    for (var t = 0; t < tracks[i].length; t++) {
-                        var repeat = tracks[i][t].blocks / _self.samples[tracks[i][t].piece].sampleLength;
-                        for (var x = 0; x < repeat; x++) {
-                            actualTrack.push(tracks[i][t].piece);
-                            for (var l = 0; l < _self.samples[tracks[i][t].piece].sampleLength - 1; l++) {
-                                actualTrack.push("0");
-                            }
-
-                        }
-                    }
-                    _self.tracks[i].playlist = actualTrack;
-                }
-                _self.CalculatePlaytime();
-                _self.OnReady();
-
-            });
-
-        }).catch(function(err) {
-            console.log("Failed during preload");
-        });*/
-
     }
 
     CalculatePlaytime() {
@@ -336,7 +295,9 @@ class TraxPlayer {
     Time() {
         this.timeInSeconds = this.timeInSeconds + 1;
         if (this.timeInSeconds > this.traxLengthInSeconds) {
+            this.playing = false;
             this.ResetPlayer();
+            this.Play();
         } else {
             this.SetPlayTime();
         }
